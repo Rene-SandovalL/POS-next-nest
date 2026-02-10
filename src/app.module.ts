76edebeca63ffcce691cc907edc229b34024ed9f
shalking <renefs127@gmail.com>
2026-02-10 00:42:20 -0700

    typeORM funcionando

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { typeOrmConfig } from './config/typeorm.config';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: typeOrmConfig,
+      inject: [ConfigService],
+    }),
+    CategoriesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,17 @@
+import { ConfigService } from '@nestjs/config';
+import type { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig = (
+  configService: ConfigService,
+): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  host: configService.get<string>('DATABASE_HOST'),
+  port: configService.get<number>('DATABASE_PORT'),
+  username: configService.get<string>('DATABASE_USER'),
+  password: configService.get<string>('DATABASE_PASSWORD'),
+  database: configService.get<string>('DATABASE_NAME'),
+
+  logging: true,
+  autoLoadEntities: true,
+  synchronize: true,
+});
